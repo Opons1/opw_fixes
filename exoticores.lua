60cfdf2b076777bceff33a298346db564e49c69b
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-01-16 12:29:11 -0800

    nerf tools

--- exoticores.lua
+++ exoticores.lua
@@ -1,7 +1,5 @@
+--These are untested, probably needs better balance
 --ARMOR
---ARMOR
---ARMOR
-
 --NEVADAITE
 --{armor_head=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
 core.override_item("exoticores:helmet_nevadaite", {
@@ -70,20 +68,112 @@ core.override_item("exoticores:boots_edoylerite", {
 core.override_item("exoticores:shield_edoylerite", {
 groups = {armor_shield=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
 })
---LONSDALETITE
+--lonsdaleite
 --{armor_head=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
-core.override_item("exoticores:helmet_lonsdaletite", {
+core.override_item("exoticores:helmet_lonsdaleite", {
     groups = {armor_head=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
 })
-core.override_item("exoticores:chestplate_lonsdaletite", {
+core.override_item("exoticores:chestplate_lonsdaleite", {
     groups = {armor_torso=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
 })
-core.override_item("exoticores:leggings_lonsdaletite", {
+core.override_item("exoticores:leggings_lonsdaleite", {
     groups = {armor_legs=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
 })
-core.override_item("exoticores:boots_lonsdaletite", {
+core.override_item("exoticores:boots_lonsdaleite", {
     groups = {armor_feet=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
 })
-core.override_item("exoticores:shield_lonsdaletite", {
+core.override_item("exoticores:shield_lonsdaleite", {
 groups = {armor_shield=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
+
+--TOOLS
+
+--NEVADAITE
+core.override_item("exoticores:sword_nevadaite", {
+    tool_capabilities = {
+		full_punch_interval = 0.5,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=100, maxlevel=3},
+		},
+		damage_groups = {fleshy=8},
+	}
+})
+--COBALTOMINITE
+core.override_item("exoticores:sword_cobaltominite", {
+      tool_capabilities = {
+		full_punch_interval = 0.4,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=1.60, [2]=0.70, [3]=0.30}, uses=120, maxlevel=3},
+		},
+		damage_groups = {fleshy=9},
+	}      
+})
+--SIDERONATRITE
+core.override_item("exoticores:sword_sideronatrite", {
+    tool_capabilities = {
+		full_punch_interval = 0.35,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=1.30, [2]=0.50, [3]=0.250}, uses=16000, maxlevel=3},
+		},
+		damage_groups = {fleshy=11},
+	}
+})
+--EDOYLERITE
+core.override_item("exoticores:edoylerite", {
+    tool_capabilities = {
+		full_punch_interval = 0.3,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=1, [2]=.3, [3]=0.20}, uses=3200, maxlevel=3},
+		},
+		damage_groups = {fleshy=12},
+	}
+})
+--lonsdaleite
+--everything was crazy, minimum 100 damage 0.01 cooldown?????
+core.override_item("exoticores:pick_lonsdaleite", {
+    tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			cracky = {times={[1]=0.09, [2]=0.18, [3]=0.27}, uses=6000, maxlevel=3},
+		},
+		damage_groups = {fleshy=12},
+	},
+})
+
+core.override_item("exoticores:shovel_lonsdaleite", {
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=0.06, [2]=0.12, [3]=0.18}, uses=6000, maxlevel=3},
+		},
+		damage_groups = {fleshy=10},
+	},
+})
+
+core.override_item("exoticores:axe_lonsdaleite", {
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=1,
+		groupcaps={
+			choppy={times={[1]=.08, [2]=0.1, [3]=0.12}, uses=6000, maxlevel=2},
+		},
+		damage_groups = {fleshy=14},
+	},
+})
+
+core.override_item("exoticores:sword_lonsdaleite", {
+	tool_capabilities = {
+		full_punch_interval = 0.25,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=0.01, [2]=0.01, [3]=0.01}, uses=6000, maxlevel=10},
+		},
+		damage_groups = {fleshy=15},
+	}
 })
