4cdf5b3575bddb2b327e37984cf3b30571bc2ac5
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-01-16 10:37:26 -0800

    nerf armor from mod "exoticores"

---
+++ exoticores.lua
@@ -0,0 +1,89 @@
+--ARMOR
+--ARMOR
+--ARMOR
+
+--NEVADAITE
+--{armor_head=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+core.override_item("exoticores:helmet_nevadaite", {
+    groups = {armor_head=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+})
+core.override_item("exoticores:chestplate_nevadaite", {
+    groups = {armor_torso=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+})
+core.override_item("exoticores:leggings_nevadaite", {
+    groups = {armor_legs=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+})
+core.override_item("exoticores:boots_nevadaite", {
+    groups = {armor_feet=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+})
+core.override_item("exoticores:shield_nevadaite", {
+    groups = {armor_shield=13,armor_heal=8,physics_jump=0.1,armor_fire=1,armor_use=200,physics_gravity=-.01}
+})
+--COBALTOMINITE
+--{armor_head=18, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+core.override_item("exoticores:helmet_cobaltominite", {
+    groups = {armor_head=16, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+})
+core.override_item("exoticores:chestplate_cobaltominite", {
+    groups = {armor_torso=16, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+})
+core.override_item("exoticores:leggings_cobaltominite", {
+    groups = {armor_legs=16, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+})
+core.override_item("exoticores:boots_cobaltominite", {
+    groups = {armor_feet=16, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+})
+core.override_item("exoticores:shield_cobaltominite", {
+groups = {armor_shield=16, armor_heal=10, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02}
+})
+--SIDERONATRITE
+--{armor_head=18.4, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+core.override_item("exoticores:helmet_sideronatrite", {
+    groups = {armor_head=18, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+})
+core.override_item("exoticores:chestplate_sideronatrite", {
+    groups = {armor_torso=18, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+})
+core.override_item("exoticores:leggings_sideronatrite", {
+    groups = {armor_legs=18, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+})
+core.override_item("exoticores:boots_sideronatrite", {
+    groups = {armor_feet=18, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+})
+core.override_item("exoticores:shield_sideronatrite", {
+groups = {armor_shield=18, armor_heal=12, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04}
+})
+--EDOYLERITE
+--{armor_head=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+core.override_item("exoticores:helmet_edoylerite", {
+    groups = {armor_head=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+})
+core.override_item("exoticores:chestplate_edoylerite", {
+    groups = {armor_torso=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+})
+core.override_item("exoticores:leggings_edoylerite", {
+    groups = {armor_legs=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+})
+core.override_item("exoticores:boots_edoylerite", {
+    groups = {armor_feet=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+})
+core.override_item("exoticores:shield_edoylerite", {
+groups = {armor_shield=18.8, armor_heal=14, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08}
+})
+--LONSDALETITE
+--{armor_head=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+core.override_item("exoticores:helmet_lonsdaletite", {
+    groups = {armor_head=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
+core.override_item("exoticores:chestplate_lonsdaletite", {
+    groups = {armor_torso=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
+core.override_item("exoticores:leggings_lonsdaletite", {
+    groups = {armor_legs=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
+core.override_item("exoticores:boots_lonsdaletite", {
+    groups = {armor_feet=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
+core.override_item("exoticores:shield_lonsdaletite", {
+groups = {armor_shield=19.2, armor_heal=16, physics_jump=0.15, physics_speed=0.2, armor_fire=10, armor_use=10,physics_gravity=-.16}
+})
--- init.lua
+++ init.lua
@@ -1,83 +1,10 @@
+--modpath
+local mp = core.get_modpath("opw_fixes")
+--mods
 local colorcubes = core.get_modpath("colorcubes")
 local unifieddyes = core.get_modpath("unifieddyes")
 local exoticores = core.get_modpath("exoticores")
-if exoticores then
- core.clear_craft({
-    output = "exoticores:lonsdaleite_crystal"
-})
-end
---Colorcubes fix
-if colorcubes and unifieddyes then
---for color cubes to have recipes
-local function add_unicolor_groups(name, color)
-  local node_def = core.registered_items[name]
-  if not node_def then return end
-  --this table copy part was assisted by ai, before i thought making a local would make a copy of a table.
-  local groups = {}
-  if node_def.groups then
-      for k, v in pairs(node_def.groups) do
-           groups[k] = v
-      end
-  end
-  groups["unicolor_" .. color] = 1
-  groups["not_in_creative_inventory"] = 0
-  core.override_item(name, {
-    groups = groups
-  })
-end
-
-add_unicolor_groups("dye:spring", "aqua")
-add_unicolor_groups("dye:black", "black")
-add_unicolor_groups("dye:blue", "blue")
-add_unicolor_groups("dye:dark_orange", "dark_orange")
-add_unicolor_groups("dye:magenta", "magenta")
-add_unicolor_groups("dye:violet", "violet")
-add_unicolor_groups("dye:dark_green", "dark_green")
-add_unicolor_groups("dye:green", "green")
-add_unicolor_groups("dye:chartreuse", "lime")
-add_unicolor_groups("dye:yellow", "yellow")
-add_unicolor_groups("dye:red", "red")
-add_unicolor_groups("dye:orange", "orange")
-add_unicolor_groups("dye:white", "white")
-add_unicolor_groups("dye:cyan", "cyan")
-add_unicolor_groups("dye:dark_grey", "darkgrey")
-add_unicolor_groups("dye:grey", "grey")
-add_unicolor_groups("dye:pink", "light_red")
-add_unicolor_groups("dye:azure", "sky_blue")
-add_unicolor_groups("dye:rose", "red_violet")
---recipes for white_single, white_light, and white_window
-local w = "dye:white"
-local t = "default:torch"
-local b = "dye:black"
-local g = "default:glass"
-core.register_craft({
-  type = "shaped",
-  output = "colorcubes:white_single",
-  recipe = {
-      {w, w, w},
-      {w, b, w},
-      {w, w, w},
-    } 
-  }
-)
-core.register_craft({
-    type = "shaped",
-    output = "colorcubes:white_window",
-    recipe = {
-      {w, w, w},
-      {w, g, w},
-      {w, w, w}
-    }
-  }
-)
-core.register_craft({
-    type = "shaped",
-    output = "colorcubes:white_light",
-    recipe = {
-      {w, w, w},
-      {w, t, w},
-      {w, w, w}
-    }
-  }
-)
-end
+--Exoticores modifications to spawn rates and nerfs
+if exoticores then dofile(mp.."/exoticores.lua") end
+--Colorcubes fix to add recipes for the dyed versions
+if colorcubes and unifieddyes then dofile(mp.."/colorcubes.lua") end
