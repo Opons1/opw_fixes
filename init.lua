c857ab3f0ee8eb69b2d9595388011823f914de6f
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-01-19 17:15:53 -0800

    Fix

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local colorcubes = core.get_modpath("colorcubes")
 local unifieddyes = core.get_modpath("unifieddyes")
 local exoticores = core.get_modpath("exoticores")
 local oak_steampunk = core.get_modpath("oak_steampunk")
+local oak = core.get_modpath("oak")
 --Exoticores modifications to spawn rates and nerfs
 if exoticores then dofile(mp.."/exoticores.lua") end
 --Colorcubes fix to add recipes for the dyed versions
@@ -12,4 +13,9 @@ if colorcubes and unifieddyes then dofile(mp.."/colorcubes.lua") end
 --crashes without tubelib
 if oak_steampunk then 
   core.unregister_item("oak_steampunk:switch_off")
+end
+if oak_steampunk and oak then
+  core.register_alias_force("oak_steampunk:oak_tree_trunk", "oak:trunk")
+  core.register_alias_force("oak_steampunk:oak_tree_planks", "oak:wood")
+  core.register_alias_force("oak_steampunk:oak_tree_leaves", "oak:leaves")
 end
