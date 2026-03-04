8a315d05e22c60afa0f50783325c7cc54be949cb
Opons1 <tazimc1001@gmail.com>
2026-03-03 18:27:05 -0800

    remove oak_steampunk since ima fork

--- init.lua
+++ init.lua
@@ -4,18 +4,7 @@ local mp = core.get_modpath("opw_fixes")
 local colorcubes = core.get_modpath("colorcubes")
 local unifieddyes = core.get_modpath("unifieddyes")
 local exoticores = core.get_modpath("exoticores")
-local oak_steampunk = core.get_modpath("oak_steampunk")
-local oak = core.get_modpath("oak")
 --Exoticores modifications to spawn rates and nerfs
 if exoticores then dofile(mp.."/exoticores.lua") end
 --Colorcubes fix to add recipes for the dyed versions
 if colorcubes and unifieddyes then dofile(mp.."/colorcubes.lua") end
---crashes without tubelib
-if oak_steampunk then 
-  core.unregister_item("oak_steampunk:switch_off")
-end
-if oak_steampunk and oak then
-  core.register_alias_force("oak_steampunk:oak_tree_trunk", "oak:trunk")
-  core.register_alias_force("oak_steampunk:oak_tree_planks", "oak:wood")
-  core.register_alias_force("oak_steampunk:oak_tree_leaves", "oak:leaves")
-end
