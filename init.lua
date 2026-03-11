15d6afe126542a0903520b7c971e0a4017c50e86
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-03-11 16:58:03 -0700

    Modify xdecor bowl recipe for wooden bucket
    Change the xdecor bowl recipe to use wooden buckets.

--- init.lua
+++ init.lua
@@ -4,7 +4,22 @@ local mp = core.get_modpath("opw_fixes")
 local colorcubes = core.get_modpath("colorcubes")
 local unifieddyes = core.get_modpath("unifieddyes")
 local exoticores = core.get_modpath("exoticores")
+local xdecor = core.get_modpath("xdecor")
 --Exoticores modifications to spawn rates and nerfs
 if exoticores then dofile(mp.."/exoticores.lua") end
 --Colorcubes fix to add recipes for the dyed versions
 if colorcubes and unifieddyes then dofile(mp.."/colorcubes.lua") end
+--change xdecor bowl recipe for wooden bucket
+if xdecor then
+  core.clear_craft({
+      output = "xdecor:bowl"
+  })
+  core.register_craft({
+      output = "xdecor:bowl 3"
+      recipe = {
+        {"group:wood","","group:wood"},
+        {"","group:slab",""},
+        {"","",""},
+      }
+  })
+end
