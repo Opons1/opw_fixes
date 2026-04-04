f9232ab16f4e0e2778c958bb16ad1b21b5d9c381
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-04-04 11:11:48 -0700

    Add crafting recipes for bottomless chests locked

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ local darkage = core.get_modpath("darkage")
 local twchests = core.get_modpath("twchests")
 if technic and twchests then
     core.clear_craft({output = "twchests:chest_bottomless"})
+    core.clear_craft({output = "twchests:chest_bottomless_locked"})
     core.register_craft({
         output = "twchests:chest_bottomless",
         recipe = {
@@ -26,6 +27,12 @@ if technic and twchests then
             {"technic:mithril_chest", "default:diamondblock", "technic:mithril_chest"},
         }
     })
+    core.register_craft({
+            type = "shapeless",
+            output = "twchests:chest_bottomless_locked",
+            recipe = {"default:steel_ingot", "twchests:chest_bottomless"},
+    )}
+            
 end
 if darkage and technic then
     core.register_craft({
